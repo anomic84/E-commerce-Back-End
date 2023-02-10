2d4c9f4ae00d76e6844446746d5c476bf96b92ce
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-02-10 14:45:47 -0800

    defined columns in Category and started working on catagory-seeds

---
+++ routes/api/category-routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const catagoryData = await Category.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(catagoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+  // find one category by its `id` value
+  // be sure to include its associated Products
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
