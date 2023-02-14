3a24ad6c3ebb147aea32c4c0a9e347ffb429f2f7
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-02-13 17:22:17 -0800

    push before office hours to meet with TA, a little lost.

--- models/index.js
+++ models/index.js
@@ -5,13 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
 
+})
 // Categories have many Products
+Category.hasMany(Product, {
 
+    through: {
+      model: Trip,
+      unique: false
+    },
+ 
+    as: 'product_catagories'
+  });
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+    through: {
+        model: ProductTag,
+        unique: false
+      },
+});
 // Tags belongToMany Products (through ProductTag)
-
+Product.belongsToMany(Tag, {
+    through: {
+        model: ProductTag,
+        unique: false
+      },
+});
 module.exports = {
   Product,
   Category,
