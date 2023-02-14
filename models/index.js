f52758f18e5e6c9cd92bf9223d0217918e56f308
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-02-13 17:23:53 -0800

    added what I think is the correct way to link the Models index

--- models/index.js
+++ models/index.js
@@ -7,34 +7,40 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
 
+    through: {
+        model: Category,
+        unique: false
+    },
+
+    as: 'product_catagories'
 })
 // Categories have many Products
 Category.hasMany(Product, {
 
     through: {
-      model: Trip,
-      unique: false
+        model: Product,
+        unique: false
     },
- 
+
     as: 'product_catagories'
-  });
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
     through: {
         model: ProductTag,
         unique: false
-      },
+    },
 });
 // Tags belongToMany Products (through ProductTag)
 Product.belongsToMany(Tag, {
     through: {
         model: ProductTag,
         unique: false
-      },
+    },
 });
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
