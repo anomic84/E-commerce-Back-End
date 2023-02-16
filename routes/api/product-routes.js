890de77a256453d9ca099f398d1db976353d8a84
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-02-16 13:05:07 -0800

    forgot to add 404 message for get product by ID, fixed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,18 +34,22 @@ router.get('/:id', async (req, res) => {
     // be sure to include its associated Category and Tag data
     try {
         const productData = await Product.findByPk(req.params.id, {
-            attributes: ['id', 'product_name', 'price', 'stock'],
+            // attributes: ['id', 'product_name', 'price', 'stock'],
             include: [
                 {
                     model: Category,
-                    attributes: ['category_name']
+                    // attributes: ['category_name']
                 },
                 {
                     model: Tag,
-                    attributes: ['tag_name']
+                    // attributes: ['tag_name']
                 }
             ]
-        })
+        });
+        if (!productData) {
+            res.status(404).json({ message: 'No Product found with that id!' });
+            return;
+        }
         res.status(200).json(productData)
     } catch (err) {
         res.status(500).json(err)
